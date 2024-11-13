ab8bb3430b8123150286c940217e894a5f464c45
Ayomide Bajo <oluwashinabajo@gmail.com>
2024-11-13 09:31:56 +0100

    convert return type from json to html

--- src/index.ts
+++ src/index.ts
@@ -28,52 +28,60 @@ async function getChatCompletion(apiKey: string, model: string, chatQuery: strin
   return result
 }
 
-app.get('/', async (c) => {
-  let vault: Record<string, string> = {}
-  let queries = c.req.queries() || {}
+app.get("/", async (c) => {
+  let vault: Record<string, string> = {};
+  let queries = c.req.queries() || {};
   try {
-    vault = JSON.parse(process.env.secret || '')
+    vault = JSON.parse(process.env.secret || "");
   } catch (e) {
-    console.error(e)
-    return c.json({ error: "Failed to parse secrets" })
+    console.error(e);
+    return c.json({ error: "Failed to parse secrets" });
   }
-  const apiKey = (vault.apiKey) ? vault.apiKey : 'sk-qVBlJkO3e99t81623PsB0zHookSQJxU360gDMooLenN01gv2'
+  const apiKey = vault.apiKey
+    ? vault.apiKey
+    : "sk-oMNgY7zegK6wUDgVA3mtYr1TmENUjOJFzgw6nM4genvAq4lK";
   // Choose from any model listed here https://docs.red-pill.ai/get-started/supported-models
-  const model = (queries.model) ? queries.model[0] : 'gpt-4o'
-  const chatQuery = (queries.chatQuery) ? queries.chatQuery[0] : 'Who are you?'
+  const model = queries.model ? queries.model[0] : "gpt-4o";
+  const chatQuery = queries.chatQuery ? queries.chatQuery[0] : "Who are you?";
   let result = {
     model,
     chatQuery: chatQuery,
-    message: ''
+    message: "",
   };
 
-  result.message = await getChatCompletion(apiKey, model, chatQuery)
+  result.message = await getChatCompletion(apiKey, model, chatQuery);
 
-  return c.json(result)
-})
+  // return c.json(result);
+  return c.html(
+    `<p>Model: ${result.model} </p> <p>Query: ${result?.chatQuery}</p> <p>Response: ${result.message}</p>`
+  );
+});
 
-app.post('/', async (c) => {
-  let vault: Record<string, string> = {}
-  const data = await c.req.json()
-  console.log('user payload in JSON:', data)
+app.post("/", async (c) => {
+  let vault: Record<string, string> = {};
+  const data = await c.req.json();
+  console.log("user payload in JSON:", data);
   try {
-    vault = JSON.parse(process.env.secret || '')
+    vault = JSON.parse(process.env.secret || "");
   } catch (e) {
-    console.error(e)
-    return c.json({ error: "Failed to parse secrets" })
+    console.error(e);
+    return c.json({ error: "Failed to parse secrets" });
   }
-  const apiKey = (vault.apiKey) ? vault.apiKey : 'sk-qVBlJkO3e99t81623PsB0zHookSQJxU360gDMooLenN01gv2'
-  const model = (data.model) ? data.model : 'gpt-4o'
-  const chatQuery = (data.chatQuery) ? data.chatQuery : 'Who are you?'
+  const apiKey = vault.apiKey
+    ? vault.apiKey
+    : "sk-oMNgY7zegK6wUDgVA3mtYr1TmENUjOJFzgw6nM4genvAq4lK";
+  const model = data.model ? data.model : "gpt-4o";
+  const chatQuery = data.chatQuery ? data.chatQuery : "Who are you?";
   let result = {
     model,
     chatQuery: chatQuery,
-    message: ''
+    message: "",
   };
 
-  result.message = await getChatCompletion(apiKey, model, chatQuery)
+  result.message = await getChatCompletion(apiKey, model, chatQuery);
 
-  return c.json(result)
+  return c.json(result);
+  // return c.html(`<p>what the hell? </p>`);
 });
 
 export default handle(app)
